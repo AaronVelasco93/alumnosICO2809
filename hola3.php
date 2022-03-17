2dd2a434f627abc843b7da10a3bdb9f19d769448
AaronVelasco93 <aaronvelasco1993@outlook.com>
2022-03-17 10:24:28 -0600

    cambio de header

--- hola3.php
+++ hola3.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+include 'header.php';
+?>
 
-
-     <!--Import Google Icon Font-->
-     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-     <!--Import materialize.css-->
-     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-     <!--Let browser know website is optimized for mobile-->
-     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="icon" href="img/logoIco.jpg">
-     
-    <title>Document</title>
-</head>
 <body>
 
     
